67eaeb95893bcb352ef40cf491a34b7b261c3e19
play.js <git@gituser.com>
2021-07-15 10:42:58 +0900

    original

---
+++ index.js
@@ -0,0 +1,17 @@
+function add(a, b, callback){
+  var result = a + b;
+  callback(result);
+}
+
+add(10, 10, function(result){
+  console.log('결과 값은 : ' + result);
+})
+
+function myFunc1(num, callback){
+ var price = num * 1040;
+ callback(price);
+}
+
+myFunc1(100, function(price){
+  console.log('결과 값은 :' + price);
+})
