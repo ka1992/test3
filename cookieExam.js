03f68c056d3533172f267c38f0d950abdc999d46
ka1992 <goodtaesik@gmail.com>
2021-07-17 18:25:31 +0900

    change contents

--- cookieExam.js
+++ cookieExam.js
@@ -1,8 +1,11 @@
 var http = require('http');
-var server = http.createServer(function(response, request){
-  response.end('cookie');
+var server = http.createServer(function(req, res){
+  res.writeHead(200, {
+    'Set-Cookie':['chco'='item1', 'macchco'='item2'];
+  });
+  res.write('<h1>cookie 생성</h1>');
+  res.end();
 });
-
 var port = 3000;
 server.listen(port, function(){
   console.log('웹서버가 켜졌습니다. : ' + port);
